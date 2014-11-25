d6d25060cd02136b7eb99f7f5b3ff9fe7bad5179
tristan.bardey@gmail.com <tristan.bardey@gmail.com@b36fca52-2207-6e1c-0eb2-a4ac728cd360>
2014-11-25 15:48:20 +0000

    one day of work

---
+++ trunk/chat-system/src/chat/system/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.system;
+import NI.*;
+
+/**
+ *
+ * @author bardey
+ */
+public class Test {
+	
+	public static void main(String[] args){
+		NetworkInterface nI = new NetworkInterface();
+		
+		
+		
+	}
+}
