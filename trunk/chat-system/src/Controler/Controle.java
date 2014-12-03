4809bd7b374389f444947eb4c649ef055bcce4ae
dauriac.al@gmail.com <dauriac.al@gmail.com@b36fca52-2207-6e1c-0eb2-a4ac728cd360>
2014-12-03 00:30:39 +0000

    ajout gui

--- trunk/chat-system/src/Controler/Controle.java
+++ trunk/chat-system/src/Controler/Controle.java
@@ -6,18 +6,24 @@ package Controler;
 import signals.*;
 import chat.system.*;
 import Model.*;
+import Gui.*;
 
+import java.awt.event.*;
 import NI.MyNetworkInterface;
 
 /**
  *
  * @author bardey
  */
-public class Controle {
+public class Controle implements ActionListener {
 	private MyNetworkInterface nI;
 	private ChatSystem cS;
 	private RemoteUsers others;
 	private Me me;
+        private Interface i;
+        private Accueil a;
+        private boolean connected = false;
+        public String username;
 	
 	public Controle (MyNetworkInterface nI, RemoteUsers o){
 		this.nI = nI;
@@ -32,6 +38,18 @@ public class Controle {
 			this.nI.sendHello(me.getUserName());
 		}
 	}
+        
+        public void setInterface(Interface i){
+            this.i = i;
+        }
+        
+        public void setAccueil(Accueil a){
+            this.a = a;
+        }
+        
+        public void setUsername(String s){
+            i.getUsernameLabel().setText(s);
+        }
 	
 	
 	private boolean checkUserName(String uN){
@@ -43,6 +61,7 @@ public class Controle {
 		String tmp []= userName.split("@");
 		others.addRemoteUser(userName,tmp[1]);
 		System.out.println("**** "+userName+" is added to the list of users at the adresse "+tmp[1]);
+                i.getUsersTextArea().append(userName+"\n");
 		try{
 			nI.sendHelloOK(me.getUserName(),others.getRemoteUserAdress(userName));
 		}catch (Exception e){
@@ -54,10 +73,12 @@ public class Controle {
 		String sender = ((TextMessage) hy).getFrom();
 		String message = ((TextMessage) hy).getMessage();
 		System.out.println("["+sender+"] :::: "+message);
+                i.getConversationTextArea().append("Received from "+sender+" : "+message+"\n");
 	} 
 	public void byeReceived(Signal bye){
 			String idiot = ((Goodbye) bye).getUsername();
-			others.killRemoteUser(idiot);		
+			others.killRemoteUser(idiot);	
+                        //enlever le mec de la liste des users connectés
 	}
 	
 	
@@ -67,21 +88,41 @@ public class Controle {
 		String tmp []= userName.split("@");
 		others.addRemoteUser(userName,tmp[1]);
 		System.out.println("**** "+userName+" is added to the list of users at the adresse "+tmp[1]);
+                i.getUsersTextArea().append(userName+"\n");
 	}
 	
 	public void sendMessage(String message, String remoteUser){
 		String remoteAddr = others.getRemoteUserAdress(remoteUser);
 		System.out.println("");
 		nI.sendTextMessage(message, remoteAddr, null);
+                i.getConversationTextArea().append("Sent to "+i.getRemoteTextField().getText()+" : "+i.getMessageTextArea().getText()+"\n");
+                i.getMessageTextArea().setText("");
 		
 	}
 	
 	public void sendBye(){
 		nI.sendBy(me.getUserName());
 	}
-	
-	
-	
-	
-	
+        
+        public boolean getConnected(){
+            return this.connected;
+        }
+        
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == a.getConnectButton()){
+                connect(a.getUsernameTextArea().getText());
+                username=a.getUsernameTextArea().getText();
+                a.setVisible(false);
+                this.connected=true;
+            }
+            
+            else if(e.getSource() == i.getSendButton()){
+                sendMessage(i.getMessageTextArea().getText(),i.getRemoteTextField().getText());  
+            }
+            
+            else if (e.getSource() == i.getDisconnectButton()){
+                //se déconnecter
+            }
+            
+        }
 }
